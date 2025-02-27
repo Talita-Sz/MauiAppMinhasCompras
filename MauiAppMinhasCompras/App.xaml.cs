3881ab0a783d83b6cfc2af69beac10bdb2438ef2
taica <taica@TALITA>
2025-02-27 16:24:58 -0300

    Add recursos ao app

--- MauiAppMinhasCompras/App.xaml.cs
+++ MauiAppMinhasCompras/App.xaml.cs
@@ -6,7 +6,10 @@
         {
             InitializeComponent();
 
-            MainPage = new AppShell();
+            //MainPage = new AppShell(); nosso objeto navigtion page recebe um novo objeto para renderizar
+            //é esperado uma nova tela para a ListaProduto
+            // até agora foi criada uma classe model e alteramos a tela inicial
+            MainPage = new NavigationPage(new Views.ListaProduto());
         }
     }
 }
--- MauiAppMinhasCompras/Models/Produto.cs
+++ MauiAppMinhasCompras/Models/Produto.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using SQLite;
+﻿using SQLite;
 
 namespace MauiAppMinhasCompras.Models
 {
@@ -13,6 +8,6 @@ namespace MauiAppMinhasCompras.Models
         public int Id { get; set; }
         public string Descricao{ get; set; }
         public double Quantidade { get; set; }
-        public double Preco {  get; set; }
+        public double Preco { get; set; }
     }
 }
